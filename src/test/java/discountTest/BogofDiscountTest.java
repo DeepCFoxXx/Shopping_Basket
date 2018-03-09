c33aab136a4fc212960dc64a08025501451c47f6
DeepCFoxXx <learncode23@gmail.com>
2018-03-09 16:06:27 +0000

    added 1st test for bogof

--- src/test/java/discountTest/BogofDiscountTest.java
+++ src/test/java/discountTest/BogofDiscountTest.java
@@ -1,4 +1,20 @@
 package discountTest;
 
+import basket.Item;
+import discount.BogofDiscount;
+import discount.Discount;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class BogofDiscountTest {
+
+    @Test public void zeroGivenIfNoBogof() {
+        Discount bogof = new BogofDiscount();
+        ArrayList<Item> items = new ArrayList<Item>();
+        assertEquals(0.0, bogof.totalDiscount(items, 0.0), 0.001);
+    }
+
 }
