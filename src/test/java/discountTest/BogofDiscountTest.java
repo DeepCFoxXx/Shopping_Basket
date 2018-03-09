f862ce6c0cc9de18e9caaa215e4a7182f907cd77
DeepCFoxXx <learncode23@gmail.com>
2018-03-09 16:26:52 +0000

    added 2nd bogof test

--- src/test/java/discountTest/BogofDiscountTest.java
+++ src/test/java/discountTest/BogofDiscountTest.java
@@ -17,4 +17,16 @@ public class BogofDiscountTest {
         assertEquals(0.0, bogof.totalDiscount(items, 0.0), 0.001);
     }
 
+    @Test
+    public void shouldGiveSavingIfTwoBogofItems(){
+        BogofDiscount bogof = new BogofDiscount();
+        bogof.addDiscountItem("Pasta");
+        ArrayList<Item> items = new ArrayList<Item>();
+        Item pasta1 = new Item("Pasta", 1.50);
+        Item pasta2 = new Item("Pasta", 1.50);
+        items.add( pasta1 );
+        items.add( pasta2 );
+        assertEquals( 3.00, bogof.totalDiscount( items, 0.0  ), 0.001 );
+    }
+
 }
